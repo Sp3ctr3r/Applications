84088b308ce2d02a824107b84ca5401d36be4352
Sp3ctr3r <hakops82@gmail.com>
2022-04-19 18:27:58 +0300

    elevator application

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace Asansör_uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(134, 474);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 63);
+            this.label4.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.Location = new System.Drawing.Point(134, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 63);
+            this.label6.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Aqua;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(435, 640);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(47, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(139, 580);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 79);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "1.Kat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(177, 18);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 26);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "0";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(266, 58);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(387, 686);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(18, 18);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(153, 26);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Kat numarası :";
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label11.Location = new System.Drawing.Point(134, 310);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(126, 63);
+            this.label11.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(139, 665);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 79);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "3.Kat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(12, 665);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 79);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "2.Kat";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(12, 580);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 79);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Zemin";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(660, 783);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Asansör";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asansör_uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 3.Kat butonuna basıldığında tüm katların
+        // arkaplan rengi transparan oluyor ve kat numarasına 3 yazılıyor
+        // sonra yanında kırmızı rengi çıkıyor
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label8.Text = "3";
+            label4.BackColor = Color.Transparent;
+            label11.BackColor = Color.Transparent;
+        }
+
+        // 1.Kat butonuna basıldığında tüm katların
+        // arkaplan rengi transparan oluyor ve kat numarasına 1 yazılıyor
+        // sonra yanında kırmızı rengi çıkıyor
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label8.Text = "1";
+            label6.BackColor = Color.Transparent;
+            label11.BackColor = Color.Transparent;
+        }
+
+        // Zemin butonuna basıldığında tüm katların
+        // arkaplan rengi transparan oluyor ve kat numarasına 0 yazılıyor
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label8.Text = "0";
+            label4.BackColor = Color.Transparent;
+            label6.BackColor = Color.Transparent;
+            label11.BackColor = Color.Transparent;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label8.Text == "0")
+            {
+                // asansörü bulunduğu konumdan 0 katına indirmek için
+                // bulunduğu konumda y ekseninde 5 piksel olarak 645 piksele
+                // kadar yükseltiyoruz
+                pictureBox1.Top += 5;
+                if (pictureBox1.Location.Y >= 645)
+                {
+                    timer1.Stop();
+                }
+            }
+
+            else if (label8.Text == "1")
+            {
+                // asansörü bulunduğu konumdan 1 katına çıkarmak için
+                // bulunduğu konumda y ekseninde 5 piksel olarak 460 piksele
+                // kadar düşürüyoruz
+                pictureBox1.Top -= 5;
+                if (pictureBox1.Location.Y <= 460)
+                {
+                    timer1.Stop();
+                    label4.BackColor = Color.Red;
+                }
+            }
+            else if (label8.Text == "2")
+            {
+                // asansörü bulunduğu konumdan 2 katına çıkarmak için
+                // bulunduğu konumda y ekseninde 5 piksel olarak 285 piksele
+                // kadar düşürüyoruz
+                pictureBox1.Top -= 5;
+                if (pictureBox1.Location.Y <= 285)
+                {
+                    timer1.Stop();
+                    label11.BackColor = Color.Red;
+                }
+            }
+
+            else if (label8.Text == "3")
+            {
+                // asansörü bulunduğu konumdan 3 katına çıkarmak için
+                // bulunduğu konumda y ekseninde 5 piksel olarak 110 piksele
+                // kadar düşürüyoruz
+                pictureBox1.Top -= 5;
+                if (pictureBox1.Location.Y <= 110)
+                {
+                    timer1.Stop();
+                    label6.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 2.Kat butonuna basıldığında tüm katların
+        // arkaplan rengi transparan oluyor ve kat numarasına 2 yazılıyor
+        // sonra yanında kırmızı rengi çıkıyor
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label8.Text = "2";
+            label4.BackColor = Color.Transparent;
+            label6.BackColor = Color.Transparent;
+        }
+    }
+}
